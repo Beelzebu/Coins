76aaab251225e9d637173778f7fd895388626bdb
Beelzebu <jsuarez.lara@outlook.com>
2018-04-27 09:55:16 -0300

    Fixed SQLite wasn't using parent's data source

--- src/io/github/beelzebu/coins/common/database/SQLite.java
+++ src/io/github/beelzebu/coins/common/database/SQLite.java
@@ -35,8 +35,6 @@ import java.sql.Statement;
  */
 public final class SQLite extends CoinsDatabase {
 
-    private HikariDataSource ds;
-
     @Override
     public void setup() {
         HikariConfig hc = new HikariConfig();
@@ -57,57 +55,48 @@ public final class SQLite extends CoinsDatabase {
     @Override
     protected void updateDatabase() {
         try (Connection c = ds.getConnection(); Statement st = c.createStatement()) {
-            core.debug("A database connection was opened.");
-            try {
-                String Data
-                        = "CREATE TABLE IF NOT EXISTS `" + DATA_TABLE + "`"
-                        + "(`id` INTEGER PRIMARY KEY AUTOINCREMENT,"
-                        + "`uuid` VARCHAR(50),"
-                        + "`nick` VARCHAR(50),"
-                        + "`balance` DOUBLE,"
-                        + "`lastlogin` LONG);";
-                String Multiplier = "CREATE TABLE IF NOT EXISTS `" + MULTIPLIERS_TABLE + "`"
-                        + "(`id` INTEGER PRIMARY KEY AUTOINCREMENT,"
-                        + "`server` VARCHAR(50),"
-                        + "`uuid` VARCHAR(50),"
-                        + "`type` VARCHAR(20),"
-                        + "`amount` INTEGER,"
-                        + "`minutes` INTEGER,"
-                        + "`endtime` LONG,"
-                        + "`queue` INT,"
-                        + "`enabled` BOOLEAN);";
-                st.executeUpdate(Data);
-                core.debug("The data table was updated.");
-                st.executeUpdate(Multiplier);
-                core.debug("The multipliers table was updated");
-                if (core.getConfig().getInt("Database Version", 1) < 2) {
-                    try {
-                        if (DriverManager.getConnection("jdbc:sqlite:plugins/Coins/database.old.db").prepareStatement("SELECT * FROM Data;").executeQuery().next() && !c.prepareStatement("SELECT * FROM " + DATA_TABLE + ";").executeQuery().next()) {
-                            core.log("Seems that your database is outdated, we'll try to update this...");
-                            ResultSet res = DriverManager.getConnection("jdbc:sqlite:plugins/Coins/database.old.db").prepareStatement("SELECT * FROM Data;").executeQuery();
-                            while (res.next()) {
-                                DatabaseUtils.prepareStatement(c, SQLQuery.CREATE_USER, res.getString("uuid"), res.getString("nick"), res.getDouble("balance"), res.getLong("lastlogin")).executeUpdate();
-                                core.debug("Migrated the data for " + res.getString("nick") + " (" + res.getString("uuid") + ")");
-                            }
-                            core.log("Successfully upadated database to version 2");
+            String data
+                    = "CREATE TABLE IF NOT EXISTS `" + DATA_TABLE + "`"
+                    + "(`id` INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "`uuid` VARCHAR(50),"
+                    + "`nick` VARCHAR(50),"
+                    + "`balance` DOUBLE,"
+                    + "`lastlogin` LONG);";
+            String multiplier = "CREATE TABLE IF NOT EXISTS `" + MULTIPLIERS_TABLE + "`"
+                    + "(`id` INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "`server` VARCHAR(50),"
+                    + "`uuid` VARCHAR(50),"
+                    + "`type` VARCHAR(20),"
+                    + "`amount` INTEGER,"
+                    + "`minutes` INTEGER,"
+                    + "`endtime` LONG,"
+                    + "`queue` INT,"
+                    + "`enabled` BOOLEAN);";
+            st.executeUpdate(data);
+            st.executeUpdate(multiplier);
+            if (core.getConfig().getInt("Database Version", 1) < 2) {
+                try {
+                    if (DriverManager.getConnection("jdbc:sqlite:plugins/Coins/database.old.db").prepareStatement("SELECT * FROM Data;").executeQuery().next() && !c.prepareStatement("SELECT * FROM " + DATA_TABLE + ";").executeQuery().next()) {
+                        core.log("Seems that your database is outdated, we'll try to update it...");
+                        ResultSet res = DriverManager.getConnection("jdbc:sqlite:plugins/Coins/database.old.db").prepareStatement("SELECT * FROM Data;").executeQuery();
+                        while (res.next()) {
+                            DatabaseUtils.prepareStatement(c, SQLQuery.CREATE_USER, res.getString("uuid"), res.getString("nick"), res.getDouble("balance"), res.getLong("lastlogin")).executeUpdate();
+                            core.debug("Migrated the data for " + res.getString("nick") + " (" + res.getString("uuid") + ")");
                         }
-                        new FileManager(core).updateDatabaseVersion(2);
-                    } catch (SQLException ex) {
-                        for (int i = 0; i < 5; i++) {
-                            core.log("An error has ocurred migrating the data from the old database, check the logs ASAP!");
-                        }
-                        core.debug(ex);
-                        return;
+                        core.log("Successfully upadated database to version 2");
                     }
+                    new FileManager(core).updateDatabaseVersion(2);
+                } catch (SQLException ex) {
+                    for (int i = 0; i < 5; i++) {
+                        core.log("An error has ocurred migrating the data from the old database, check the logs ASAP!");
+                    }
+                    core.debug(ex);
+                    return;
                 }
-                if (core.getConfig().getBoolean("General.Purge.Enabled", true) && core.getConfig().getInt("General.Purge.Days") > 0) {
-                    st.executeUpdate("DELETE FROM " + DATA_TABLE + " WHERE lastlogin < " + (System.currentTimeMillis() - (core.getConfig().getInt("General.Purge.Days", 60) * 86400000L)) + ";");
-                    core.debug("Inactive users were removed from the database.");
-                }
-            } finally {
-                st.close();
-                c.close();
-                core.debug("The connection was closed.");
+            }
+            if (core.getConfig().getBoolean("General.Purge.Enabled", true) && core.getConfig().getInt("General.Purge.Days") > 0) {
+                st.executeUpdate("DELETE FROM " + DATA_TABLE + " WHERE lastlogin < " + (System.currentTimeMillis() - (core.getConfig().getInt("General.Purge.Days", 60) * 86400000L)) + ";");
+                core.debug("Inactive users were removed from the database.");
             }
         } catch (SQLException ex) {
             core.log("Something was wrong creating the default databases. Please check the debug log.");
